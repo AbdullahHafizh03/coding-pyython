acd2bed3ad87b62fea1ea0147010c69086856691
AbdullahHafizh03 <jannafirdaus89@gmail.com>
2025-03-14 17:59:44 +0800

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,19 @@
+print('hello world')
+print('welcome to python programming language')
+
+mangga = 5
+pir    = 10
+total  = mangga + pir
+print(f'{mangga} + {pir} = {total}')
+
+Nama_Lengkap = 'azzam'
+namalengkap2 = 'hafizh'
+
+print(namalengkap2)
+
+a = 8
+b = 6
+
+if a > b:
+
+
